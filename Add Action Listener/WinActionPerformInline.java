c29f4f3f3395f335f624b7772f8015a519041c9e
ARINDAM GHOSH <128517088+arindam2590@users.noreply.github.com>
2025-08-19 23:15:04 +0530

    Add files via upload

---
+++ Add Action Listener/WinActionPerformInline.java
@@ -0,0 +1,50 @@
+//This is a java program for ActionListener.
+
+import javax.swing.*; 
+import java.awt.event.*; 
+
+public class WinActionPerformInline
+{ 
+	private JFrame myframe;
+	private JPanel mypanel;
+	private JButton btn;
+	private JLabel msg;
+	
+	WinActionPerformInline(String title, int w, int h)
+	{
+		//create a object of JFrame, JPanel, JButton, JLabel class.
+		myframe = new JFrame(title);
+		mypanel = new JPanel();
+		btn = new JButton("Click Me");
+		msg = new JLabel("************Thank You***********");
+		
+		//set properties of label
+		msg.setBounds(180, 50, 300, 70);
+		msg.setVisible(false);
+		
+		//set properties of button
+		btn.setBounds(180, 50, 100, 70); 
+		
+		//add Action Listner(Click on Button) inline to the Button
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				msg.setVisible(true);
+			}
+		});
+		
+		//add component in the panel
+		mypanel.add(btn);
+		mypanel.add(msg);
+				
+		//set properties of frame
+		myframe.add(mypanel);
+		myframe.setSize(w, h);
+		myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myframe.setVisible(true);
+	}
+	
+    public static void main(String[] args) 
+    { 
+		new WinActionPerformInline("My First Window", 500, 300);
+	}
+}
